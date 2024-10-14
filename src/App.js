a3f05943e675df18e11392e36c7f04b9134fe4c7
yunseo <cj2174@naver.com>
2024-10-14 18:02:46 +0900

    초기 커밋

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Row from "react-bootstrap/Row";
+import productImage1 from "./images/product1.jpg";
+import productImage2 from "./images/product2.jpg";
+import productImage3 from "./images/product3.jpg";
+import "./App.css";
+import data from "./data.js";
+import Product from "./product.js";
+import { useState } from "react";
 
 function App() {
+  let [productList] = useState(data);
+  const productImages = [productImage1, productImage2, productImage3];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" data-bs-theme="light" className="navbar">
+        <Container>
+          <Navbar.Brand href="#home" className="navbar-brand">
+            Shop
+          </Navbar.Brand>
+          <Nav className="me-auto navbar-nav">
+            <Nav.Link href="#home" className="nav-link">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#features" className="nav-link">
+              Features
+            </Nav.Link>
+            <Nav.Link href="#pricing" className="nav-link">
+              Pricing
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+
+      <Container className="product-container">
+        <Row className="product-row">
+          {productList.map((product, index) => (
+            <Product
+              key={product.id}
+              title={product.title}
+              image={productImages[index]}
+              content={product.content}
+              price={product.price}
+            />
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,22 @@
+const product = [
+  {
+    id: 0,
+    title: "Hydrating Moisturizer",
+    content: "Perfect for daily hydration",
+    price: 120000,
+  },
+  {
+    id: 1,
+    title: "Soothing Toner",
+    content: "Calms and refreshes the skin",
+    price: 110000,
+  },
+  {
+    id: 2,
+    title: "Brightening Serum",
+    content: "Born to boost skin radiance",
+    price: 130000,
+  },
+];
+
+export default product;
